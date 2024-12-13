0a9130f9ddc3c96270ca483746017ae2aff46011
wuttiwong <wutkmutt@gmail.com>
2024-12-13 16:44:35 +0700

    user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,114 +1,120 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  signin: async (req, res) => {
-    try {
-      const rows = await prisma.user.findFirst({
-        select: {
-          id: true,
-          name: true,
-          level: true,
-        },
-        where: {
-          username: req.body.username,
-          password: req.body.password,
-          status: "use",
-        },
-      });
+    signIn: async (req, res) => {
+        try {
+            const u = req.body.username;
+            const p = req.body.password;
 
-      if (rows != null) {
-        const key = process.env.SECRET_KEY;
-        const token = jwt.sign(rows, key, { expiresIn: "30d" });
+            const user = await prisma.user.findFirst({
+                select: {
+                    id: true,
+                    name: true,
+                    level: true
+                },
+                where: {
+                    username: u,
+                    password: p,
+                    status: 'use'
+                }
+            })
 
-        return res.send({ token: token, name: rows.name });
-      }
+            if (user != null) {
+                const key = process.env.SECRET_KEY;
+                const token = jwt.sign(user, key, { expiresIn: '30d' });
 
-      return res.status(401).send({ message: "unauthorized" });
-    } catch (e) {
-      return res.status(500).send({ error: e.message });
-    }
-  },
-  list: async (req, res) => {
-    try {
-      const users = await prisma.user.findMany({
-        where: {
-          status: "use",
-        },
-        orderBy: {
-          id: "desc",
-        },
-      });
-
-      return res.send({ results: users });
-    } catch (e) {
-      return res.status(500).send({ error: e.message });
-    }
-  },
-  create: async (req, res) => {
-    try {
-      await prisma.user.create({
-        data: {
-          username: req.body.username,
-          password: req.body.password,
-          name: req.body.name,
-          level: req.body.level
-        },
-      });
-
-      return res.send({ message: "success" });
-    } catch (e) {
-      return res.status(500).send({ error: e.message });
-    }
-  },
-  remove: async (req, res) => {
-    try {
-      await prisma.user.update({
-        where: {
-          id: parseInt(req.params.id)
-        },
-        data: {
-          status: "delete"
-        },
-      });
-
-      return res.send({ message: "success" });
-    } catch (e) {
-      return res.status(500).send({ error: e.message });
-    }
-  },
-  update: async (req, res) => {
-    try {
-      await prisma.user.update({
-        where: {
-          id: req.body.id
-        },
-        data: {
-          username: req.body.username,
-          password: req.body.password,
-          name: req.body.name,
-          level: req.body.level
+                return res.send({ token: token, name: user.name, id: user.id })
+            } else {
+                return res.status(401).send();
+            }
+        } catch (e) {
+            return res.status(500).send({ error: e.message })
         }
-      });
+    },
+    create: async (req, res) => {
+        try {
+            await prisma.user.create({
+                data: {
+                    name: req.body.name,
+                    username: req.body.username,
+                    password: req.body.password,
+                    level: req.body.level
+                }
+            });
 
-      return res.send({ message: "success" });
-    } catch (e) {
-      return res.status(500).send({ error: e.message });
-    }
-  },
-  getLevelFromToken: async (req, res) => {
-    try {
-      const token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.SECRET_KEY);
-      const level = decoded.level;
+            return res.send({ message: 'success' });
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
+    },
+    list: async (req, res) => {
+        try {
+            const users = await prisma.user.findMany({
+                select: {
+                    id: true,
+                    name: true,
+                    username: true,
+                    level: true
+                },
+                where: {
+                    status: 'use'
+                },
+                orderBy: {
+                    id: 'asc'
+                }
+            });
 
-      return res.send({ level: level });
-    } catch (e) {
-      return res.status(500).send({ error: e.message });
+            return res.send({ results: users });
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
+    },
+    update: async (req, res) => {
+        try {
+            await prisma.user.update({
+                where: {
+                    id: req.body.id
+                },
+                data: {
+                    name: req.body.name,
+                    username: req.body.username,
+                    level: req.body.level
+                }
+            });
+
+            return res.send({ message: 'success' });
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
+    },
+    remove: async (req, res) => {
+        try {
+            await prisma.user.update({
+                where: {
+                    id: parseInt(req.params.id)
+                },
+                data: {
+                    status: 'delete'
+                }
+            });
+
+            return res.send({ message: 'success' });
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
+    },
+    getLevelByToken: async (req, res) => {
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const user = jwt.verify(token, process.env.SECRET_KEY);
+
+            return res.send({ level: user.level });
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
     }
-  }
-};
+}
